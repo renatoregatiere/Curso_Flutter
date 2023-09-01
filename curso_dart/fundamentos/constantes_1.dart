8ba0c19c5a3245ab01db15d9edec1271b3ac79a0
Renato Regatiere <renatoregatiere@gmail.com>
2023-09-01 13:34:16 -0300

    1

--- curso_dart/fundamentos/constantes_1.dart
+++ curso_dart/fundamentos/constantes_1.dart
@@ -2,7 +2,13 @@ import 'dart:io';
 
 main() {
   //area da circunferencia = PI * raio * raio
+  const PI = 3.1415;
 
-  String texto = .readLineSync();
-  print(texto);
+  stdout.write('Informe o raio: ');
+  final entradaDoUsuario = stdin.readLineSync();
+  final raio = double.parse(entradaDoUsuario!);
+
+  final area = PI * raio * raio;
+
+  print('o valor do raia é: ' + area.toString());
 }
