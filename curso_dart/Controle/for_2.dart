7a61fa645b1189e76da329e3f1a3ef9c8d555915
Renato Regatiere <renatoregatiere@gmail.com>
2023-09-15 21:14:23 -0300

    +

--- curso_dart/Controle/for_2.dart
+++ curso_dart/Controle/for_2.dart
@@ -1,6 +1,20 @@
 main() {
   var notas = [8.9, 6.3, 8.5, 3.7, 6.5];
-  for (var nota in notas){
-  print('O valor da nota é $nota.');
-  }  
+  for (var nota in notas) {
+    print('O valor da nota é $nota.');
+  }
+
+  var coordenadas = [
+    [1, 3],
+    [2, 4],
+    [12, 23],
+    [14, 35],
+  ];
+
+  for (var coordenada in coordenadas) {
+    print('Valor da coordenada é $coordenada');
+    for (var ponto in coordenada) {
+      print('Valor do ponto é $ponto');
+    }
+  }
 }
