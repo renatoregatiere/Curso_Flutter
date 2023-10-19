1ebec9aa904124cb54b04fe1dca1185db72ee9a3
Renato Regatiere <renatoregatiere@gmail.com>
2023-10-19 12:23:39 -0300

    +

--- curso_dart/Exercicios/exercicio.dart
+++ curso_dart/Exercicios/exercicio.dart
@@ -1,19 +1,23 @@
 
+//Método com parametro nomeado
+imprimirProduto({required String nome, required double preco}) {
+  
+  print('O produto ${nome} tem preço de R\$ ${preco}');
+}
+
 class Produto {
   String nome;
   double preco;
 
-Produto(String nome, double preco) {
-  this.nome = nome;
+  Produto({required this.nome, this.preco = 19.99});
 }
-
-}
-
 
 main() {
-  var p1 = new Produto();
-  p1.nome = 'Lapis';
-  p1.preco = 1.5;
+  var p1 = new Produto(nome: 'Lapis'); //parametro nomeado
+  var p2 = new Produto(preco: 3.99, nome: 'Geladeira');
 
-  print('O produto ${p1.nome} tem preço de R\$${p1.preco}')
+  print('O produto ${p1.nome} tem preço de R\$ ${p1.preco}');
+  print('O produto ${p2.nome} tem preço de R\$ ${p2.preco}');
+
+  imprimirProduto(preco: p1.preco, nome: p1.nome);
 }
