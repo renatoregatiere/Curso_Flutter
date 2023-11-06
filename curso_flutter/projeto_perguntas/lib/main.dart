f1dfb8dcf9cf0b679d1696b4390326392b16e8b1
Renato Regatiere <renatoregatiere@gmail.com>
2023-11-06 13:06:39 -0300

    Update main.dart

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -2,24 +2,29 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondidada!');
+  }
 
   @override
   final perguntas = [
     'Qual é a sua cor favorita?',
     'Qual é o seu animal favorito?',
-  ]
+  ];
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: const Text('Perguntas'),
         ),
         body: Column(
           children: [
             Text(perguntas[0]),
+            const ElevatedButton(onPressed: responder, child: Text('resposta 1')),
+            const ElevatedButton(onPressed: null, child: Text('resposta 2')),
+            const ElevatedButton(onPressed: null, child: Text('resposta 3')),
           ],
         ),
       ),
